fd38e320cab0f1a621240a0eed80af25e626a61a
Sreeraj <psreeraj711@gmail.com>
2025-04-02 01:31:29 +0530

    app updated

--- CineHunt/src/App.jsx
+++ CineHunt/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
   )
 }
 
