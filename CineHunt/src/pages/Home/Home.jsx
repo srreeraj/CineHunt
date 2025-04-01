e5dd90c8de9854ee268db708436c246d0200c9b7
Sreeraj <psreeraj711@gmail.com>
2025-04-02 01:28:10 +0530

    home page is set

---
+++ CineHunt/src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useAuth } from '../../context/AuthContext/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+  const { user , logout } = useAuth();
+  const navigate = useNavigate();
+
+  if (!user){
+    navigate('/login');
+    return null
+  }
+
+  return (
+    <div className='p-4'>
+        <h1 className='text-2xl'>Hello, {user.displayName}</h1>
+        <button onClick={logout} className='bg-red-500 text-white px-4 py-2 mt-4 rounded'>
+            Logout
+        </button>
+    </div>
+  )
+}
+
+export default Home
