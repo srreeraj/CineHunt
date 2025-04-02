2f9c8f234de7215e9e197fdc759f922d22f43bd5
Sreeraj <psreeraj711@gmail.com>
2025-04-03 01:37:00 +0530

    login updated

--- CineHunt/src/pages/Login/Login.jsx
+++ CineHunt/src/pages/Login/Login.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuth } from '../../context/AuthContext/AuthContext'
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
-  const { login } = useAuth();
+
+  const { user , login} = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(user) {
+      navigate('/')
+    }
+  },[user, navigate])
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
         <h1 className='text-3xl mb-4'>Welcome to CineHunt</h1>
